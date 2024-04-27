8505eaddb9ee486b250b2161f6c389eed201a68d
cybuhh <cybuhh@users.noreply.github.com>
2024-04-27 19:45:34 +0200

    fix ssh copy auth keys

--- ssh-copy-keys.sh
+++ ssh-copy-keys.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-test -f ssh-authorized_keys || exit 1
-
 CURRENT_DIR=$(dirname -- "$0")
 
+test -f "$CURRENT_DIR/ssh-authorized_keys" || exit 1
+
 test -d /home/game/.ssh || mkdir /home/game/.ssh
-cp "$CURRENT_DIR/SSH-Enabler/ssh-authorized_keys" /home/game/.ssh/authorized_keys
+cp "$CURRENT_DIR/ssh-authorized_keys" /home/game/.ssh/authorized_keys
+
+sync
+
+exit 0
