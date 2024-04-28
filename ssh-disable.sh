f77e68f32875ec95b98703ed7a0a9dca091c61c4
cybuhh <cybuhh@users.noreply.github.com>
2024-04-28 08:11:10 +0200

    remove useless install packages

--- ssh-disable.sh
+++ ssh-disable.sh
@@ -2,9 +2,6 @@
 
 type sshd 2>&1 1> /dev/null 2>/dev/null || exit 1
 
-apt update
-apt -y install sshd
-
 systemctl disable ssh &>/tmp/anbernic-ssh-service.log
 systemctl stop ssh &>/tmp/anbernic-ssh-service.log
 
